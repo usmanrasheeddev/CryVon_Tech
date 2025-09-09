7bb1bef3270e7bf3a17e4dabb22d737f06c8943d
Usman Rasheed <usmanrasheed.dev@gmail.com>
2025-09-09 00:55:34 +0000

    disabled eslint checks during build

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // ✅ build ke time ESLint errors ignore ho jayenge
-    ignoreDuringBuilds: true,
-  },
+ 
 };
 
 export default nextConfig;
